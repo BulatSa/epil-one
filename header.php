d7347429ba651d20f24339b5912fd027146ed091
Andrew <andreiduffy@gmail.com>
2018-04-30 16:06:30 +0300

    favicons \ OG

--- header.php
+++ header.php
@@ -13,20 +13,21 @@
 
 	<title>Epil One</title>
 
-	<!-- favocons -->
-	<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-	<link rel="manifest" href="favicons/manifest.json">
-	<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
-	<link rel="shortcut icon" href="favicons/favicon.ico">
-	<meta name="msapplication-config" content="favicons/browserconfig.xml">
-	<meta name="theme-color" content="#ffffff">
-	<!-- favocons -->
+	<!-- favicons -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+	<link rel="manifest" href="/favicons/site.webmanifest">
+	<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#00c3f3">
+	<link rel="shortcut icon" href="/favicons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#4edffb">
+	<meta name="msapplication-config" content="/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#4edffb">
+	<!-- favicons -->
 
 	<!-- og -->
 	<meta property="og:title" content="Epil One">
-	<meta property="og:description" content="Epil One">
+	<meta property="og:description" content="Гарантированное избавление от волос">
 	<meta property="og:type" content="website">
 	<meta property="og:image" content="/img/og.jpg">
 	<!-- og -->
